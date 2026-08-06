7ffc38e35cc0665f7db85e7552fbe50773251404
itzvy1nx <t1kt0k.d1s3lo@gmail.com>
2026-08-06 01:21:02 -0400

    Update Main.m

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"http://74.235.140.148:3000"
+#define API_URL @"http://10.0.0.92:3551"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
